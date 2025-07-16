8e61ff94c94b022ceaae40e0093c3799527134fd
honey <honeyyousaf9@gmail.com>
2025-07-16 12:31:16 +0500

    make some tweaks add new project and project img change layout structure

--- script.js
+++ script.js
@@ -1,112 +1,137 @@
+// circle follower
+let circle = document.querySelector("#circle");
 // nav & welcome text
-let nav = document.querySelector("nav")
-let welText = document.querySelector("#wel-cont")
-let header = document.querySelector("header")
-let imgcont = document.querySelector("#img-cont")
-let textcont = document.querySelector("#text-cont")
+let nav = document.querySelector("nav");
+let welText = document.querySelector("#wel-cont");
+let header = document.querySelector("header");
+let imgcont = document.querySelector("#img-cont");
+let textcont = document.querySelector("#text-cont");
 // body selector
-let body = document.querySelector("body")
+let body = document.querySelector("body");
 // side bar
-let navBtn = document.querySelector("#nav-btn")
-let sideBar = document.querySelector("#side-bar")
+let navBtn = document.querySelector("#nav-btn");
+let sideBar = document.querySelector("#side-bar");
 // side links
-let sideBarlinks = document.querySelectorAll(".side-links")
-let sideHomelink = document.querySelector("#side-home-btn")
+let sideBarlinks = document.querySelectorAll(".side-links");
+let sideHomelink = document.querySelector("#side-home-btn");
 // side bar chat btn
-let sideChatbtn = document.querySelector("#side-chat-btn")
+let sideChatbtn = document.querySelector("#side-chat-btn");
 // light icon dark icon
-let lightIcon = document.querySelector("#light")
-let darkIcon = document.querySelector("#dark")
+let lightIcon = document.querySelector("#light");
+let darkIcon = document.querySelector("#dark");
 // side bar light icon dark icon
-let sidelightIcon = document.querySelector("#side-light")
-let sidedarkIcon = document.querySelector("#side-dark")
+let sidelightIcon = document.querySelector("#side-light");
+let sidedarkIcon = document.querySelector("#side-dark");
 // theme switcher btn
-let themeSwitcherbtn = document.querySelector("#theme-switcher")
+let themeSwitcherbtn = document.querySelector("#theme-switcher");
 // side theme switcher btn
-let sidethemeSwitcherBtn = document.querySelector("#side-theme-switcher")
+let sidethemeSwitcherBtn = document.querySelector("#side-theme-switcher");
+// name / highlight1
+let blend = document.querySelector(".blend");
+// circle follower logic
+body.addEventListener("mousemove", (e) => {
+  let X = e.clientX;
+  let Y = e.clientY;
+  circle.style.transform = `translateX(${X}px) translateY(${Y}px)`;
+});
+window.addEventListener("load", () => {
+  body.style.overflowY = "hidden";
+});
 // if statement for removing wel come msg on small screens
 
-if(window.innerHeight > 480){
-    setTimeout(() => {
-    welText.style.opacity = "0"
-} , 1000)
-welText.addEventListener("transitionend" , () => {
-    welText.style.display = "none"
-   nav.classList.add("active")
-   navBtn.classList.add("active")
-   imgcont.classList.add("active")
-   textcont.classList.add("active")
-   
-}) 
+if (window.innerHeight > 480) {
+  setTimeout(() => {
+    welText.style.opacity = "0";
+  }, 1000);
+  welText.addEventListener("transitionend", () => {
+    welText.style.display = "none";
+    nav.classList.add("active");
+    navBtn.classList.add("active");
+    imgcont.classList.add("active");
+    textcont.classList.add("active");
+    body.style.overflowY = "visible";
+  });
 }
 // event for side bar
-navBtn.addEventListener("click" , () => {
-    sideBar.classList.toggle("side-active")
-    navBtn.classList.toggle("activeX")
-       setTimeout(() => {
-        sideHomelink.classList.toggle("appear")
-     },600)
-     sideBarlinks.forEach(link => {
-        setTimeout(() => {
-        link.classList.toggle("appear")
-     } , 700)
-     })
-     setTimeout(() => {
-        sideChatbtn.classList.toggle("slideinbomp")
-        sidethemeSwitcherBtn.classList.toggle("slideinbomp")
-     },800)
-}) 
+navBtn.addEventListener("click", () => {
+  sideBar.classList.toggle("side-active");
+  navBtn.classList.toggle("activeX");
+  setTimeout(() => {
+    sideHomelink.classList.toggle("appear");
+  }, 600);
+  sideBarlinks.forEach((link) => {
+    setTimeout(() => {
+      link.classList.toggle("appear");
+    }, 700);
+  });
+  setTimeout(() => {
+    sideChatbtn.classList.toggle("slideinbomp");
+    sidethemeSwitcherBtn.classList.toggle("slideinbomp");
+  }, 800);
+});
 
 // event for side bar self closing
-sideBarlinks.forEach(link => {
-    link.addEventListener("click" , () => {
-        sideBar.classList.toggle("side-active")
-        navBtn.classList.toggle("activeX")
-        sideChatbtn.classList.toggle("slideinbomp")
-        sideHomelink.classList.toggle("appear")
-        sideBarlinks.forEach(link => {
-            link.classList.toggle("appear")
-        })
-    })
-})
-sideHomelink.addEventListener("click" , () => {
-        sideBar.classList.remove("side-active")
-        navBtn.classList.remove("activeX")
-        sideHomelink.classList.remove("appear")
-        sideChatbtn.classList.remove("slideinbomp")
-       sideBarlinks.forEach(link => {
-        link.classList.remove("appear")
-       })
-})
-sidethemeSwitcherBtn.addEventListener("click" , () => {
-    sideBar.classList.remove("side-active")
-        navBtn.classList.remove("activeX")
-        sideHomelink.classList.remove("appear")
-        sideChatbtn.classList.remove("slideinbomp")
-        sidethemeSwitcherBtn.classList.remove("slideinbomp")
-       sideBarlinks.forEach(link => {
-        link.classList.remove("appear")
-       })
-})
+sideBarlinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    sideBar.classList.toggle("side-active");
+    navBtn.classList.toggle("activeX");
+    sideChatbtn.classList.toggle("slideinbomp");
+    sideHomelink.classList.toggle("appear");
+    sideBarlinks.forEach((link) => {
+      link.classList.toggle("appear");
+    });
+  });
+});
+sideHomelink.addEventListener("click", () => {
+  sideBar.classList.remove("side-active");
+  navBtn.classList.remove("activeX");
+  sideHomelink.classList.remove("appear");
+  sideChatbtn.classList.remove("slideinbomp");
+  sideBarlinks.forEach((link) => {
+    link.classList.remove("appear");
+  });
+});
+sidethemeSwitcherBtn.addEventListener("click", () => {
+  sideBar.classList.remove("side-active");
+  navBtn.classList.remove("activeX");
+  sideHomelink.classList.remove("appear");
+  sideChatbtn.classList.remove("slideinbomp");
+  sidethemeSwitcherBtn.classList.remove("slideinbomp");
+  sideBarlinks.forEach((link) => {
+    link.classList.remove("appear");
+  });
+});
 // event for switching theme
-    themeSwitcherbtn.addEventListener("click" , () => {
-    body.classList.toggle("light-theme")
-    if(body.classList.contains("light-theme")){
-           darkIcon.style.display = "none"
-           lightIcon.style.display = "inline-block"
-    }else{
-            lightIcon.style.display = "none"
-            darkIcon.style.display = "inline-block"
-    }
-})
+themeSwitcherbtn.addEventListener("click", () => {
+  body.classList.toggle("light-theme");
+  if (body.classList.contains("light-theme")) {
+    darkIcon.style.display = "none";
+    lightIcon.style.display = "inline-block";
+  } else {
+    lightIcon.style.display = "none";
+    darkIcon.style.display = "inline-block";
+  }
+});
 // event for side bar switching theme
-    sidethemeSwitcherBtn.addEventListener("click" , () => {
-    body.classList.toggle("light-theme")
-    if(body.classList.contains("light-theme")){
-           sidedarkIcon.style.display = "none"
-           sidelightIcon.style.display = "inline-block"
-    }else{
-            sidelightIcon.style.display = "none"
-            sidedarkIcon.style.display = "inline-block"
-    }
-})
+sidethemeSwitcherBtn.addEventListener("click", () => {
+  body.classList.toggle("light-theme");
+  if (body.classList.contains("light-theme")) {
+    sidedarkIcon.style.display = "none";
+    sidelightIcon.style.display = "inline-block";
+  } else {
+    sidelightIcon.style.display = "none";
+    sidedarkIcon.style.display = "inline-block";
+  }
+});
+// blend mode for cursor's follower/circle
+// blend.addEventListener('mouseenter' , () => {
+//     let width = blend.getBoundingClientRect().width
+//     let height = blend.getBoundingClientRect().height
+//     console.log(`width = ${width} height = ${height}`)
+//     circle.style.width = width + 10 + "px"
+//     circle.style.height = height + 40 + "px"
+// })
+// blend.addEventListener('mouseleave' , () => {
+// circle.style.width = 10 + "px"
+//     circle.style.height = 10 + "px"
+// })
